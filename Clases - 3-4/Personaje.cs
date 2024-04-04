4756f7fd23d75b208b5a99a8e509168a5e47119e
LorenzoGiacchetta <lorenzogiacchetta1005@gmail.com>
2024-04-03 22:51:17 -0300

    Agregar archivos de proyecto.

---
+++ Clases - 3-4/Personaje.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases___3_4
+{
+    internal class Personaje
+    {
+        public string Color { get; set; }
+        public int Altura { get; set; }
+        public int Vida { get; set; }
+        public int Defensa { get; set; }
+        public int Fuerza { get; set; }
+        public int Mana { get; set; }
+
+        public bool Atacar(Personaje personaje) {
+            if (Mana >= 10) {
+                Mana -= 10;
+                return personaje.RecibirDaño(Fuerza);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RecibirDaño(int Fuerza) {
+            int Daño = Fuerza - Defensa;
+            if (Daño > 0)
+            {
+                Vida = Vida - Daño;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public void CambiarColor(string NuevoColor) {
+            Color = NuevoColor;
+        }
+    }
+}
---
+++ Clases - 3-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases___3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personaje Jugador = new Personaje();
+            Console.WriteLine("Ingrese la Altura de su Personaje:");
+            Jugador.Altura = int.Parse(Console.ReadLine());
+            Jugador.Color = "#FFFFFF";
+            Jugador.Vida = 100;
+            Jugador.Mana = 100;
+            Jugador.Defensa = EstRandom();
+            Jugador.Fuerza = EstRandom();
+            Console.Clear();
+            Console.WriteLine("Estadisticas del Jugador:");
+            Console.WriteLine("Altura: " + Jugador.Altura);
+            Console.WriteLine("Vida: " + Jugador.Vida);
+            Console.WriteLine("Mana: " + Jugador.Mana);
+            Console.WriteLine("Defensa: " + Jugador.Defensa);
+            Console.WriteLine("Fuerz: " + Jugador.Fuerza);
+            Console.ReadLine();
+        }
+
+        static int EstRandom() {
+            int RandomNumber = new Random().Next(1, 100);
+            return RandomNumber;
+        }
+
+    }
+}
